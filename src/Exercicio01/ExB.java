508790ad8926e06c62bc164e20b96b1ebed72207
Henrique <bernardeshenrique134@gmail.com>
2026-04-27 16:56:03 -0300

    ExerciciosDaAula10
    4 Exercicios de java da aula 10

---
+++ src/Exercicio01/ExB.java
@@ -0,0 +1,10 @@
+package Exercicio01;
+
+public class ExB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Exercicio01/ExC.java
@@ -0,0 +1,10 @@
+package Exercicio01;
+
+public class ExC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
