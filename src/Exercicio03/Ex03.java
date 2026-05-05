61bd108d38f8de93b0786214ec6be9a7a799dcd3
Henrique <bernardeshenrique134@gmail.com>
2026-05-05 07:15:17 -0300

    Update Ex03.java
    Exercicio 03 completo

--- src/Exercicio03/Ex03.java
+++ src/Exercicio03/Ex03.java
@@ -1,10 +1,28 @@
 package Exercicio03;
 
+import java.util.Scanner;
+
 public class Ex03 {
+    public static void main(String[] args) {
+        Scanner receba = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("Digite uma sequência de 0 e 1:");
+        String texto = receba.nextLine();
 
-	}
+        int contador = 0; 
+        int segundocontador = 0;
 
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.charAt(i) == '1') {
+                contador++;
+            } else if (texto.charAt(i) == '0') {
+                segundocontador++;
+            }
+        }
+
+        System.out.println("Quantidade de 1: " + contador);
+        System.out.println("Quantidade de 0: " + segundocontador);
+
+        receba.close();
+    }
 }
